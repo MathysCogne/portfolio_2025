6ac6907b8060b5dbefaea21505e4d17ac62cabc8
Mathys Cogne <mathys.cognef@gmail.com>
2025-03-08 21:58:39 +0100

    Remove stats vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,6 @@ import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Navbar } from '@/components/nav'
 import { NavMobile } from '@/components/nav-mobile'
-import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import Footer from '@/components/footer'
 import { baseUrl } from '@/sitemap'
 import { texts } from '@/lib/constants'
@@ -88,8 +86,6 @@ export default function RootLayout({
             <NavMobile />
           </div>
         </LanguageProvider>
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
